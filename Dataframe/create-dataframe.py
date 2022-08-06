5096bdb245884385fc8bd6dbe6ef7cb958d809e0
seunghabaek <slug0326@naver.com>
2022-08-06 11:17:29 +0900

    create dataframe

---
+++ Dataframe/create-dataframe.py
@@ -0,0 +1,33 @@
+from os import truncate
+from tokenize import StringPrefix
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+
+# create DataFrame 
+columns = ["language", "users_count"]
+data = [("Java", "20000"), ("Python", "100000"), ("Scala", "3000")]
+
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+
+dfFromData = spark.createDataFrame(data).toDF(*columns)
+
+## create DataFrame with Schema
+data2 = [("James","","Smith","36636","M",3000),
+    ("Michael","Rose","","40288","M",4000),
+    ("Robert","","Williams","42114","M",4000),
+    ("Maria","Anne","Jones","39192","F",4000),
+    ("Jen","Mary","Brown","","F",-1)
+  ]
+
+schema = StructType([
+    StructField("firstname", StringType(), True),
+    StructField("middlename", StringType(), True),
+    StructField("lastname", StringType(), True),
+    StructField("id", StringType(), True),
+    StructField("gender", StringType(), True),
+    StructField("salary", IntegerType(), True)
+])
+
+df = spark.createDataFrame(data=data2, schema=schema)
+df.printSchema()
+df.show(truncate=False)
